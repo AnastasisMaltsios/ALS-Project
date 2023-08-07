3555f6826756981d748169a0f9a6bf23ca81fcd8
Anastasis Maltsios <myemail@email.com>
2023-08-08 00:06:00 +0300

    Algorithm Complete

--- app.js
+++ app.js
@@ -28,6 +28,44 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+const usersSchema = new mongoose.Schema ({
+  name: String,
+  lastname: String,
+  email: String,
+  phone: Number,
+  username: String,
+  password: String,
+  isAdmin: { type: Boolean, default: false },
+  patients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patient', autopopulate: true }]
+});
+
+usersSchema.plugin(passportLocalMongoose);
+usersSchema.plugin(findOrCreate);
+
+
+const User = mongoose.model("User", usersSchema);
+
+
+async function connectToDB() {
+  try {
+    await mongoose.connect("mongodb+srv://admin-anastasis:Sf6DPj6QbS4BfK4@als.eudcbjz.mongodb.net/alsDB", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB Atlas");
+
+    passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
+
 app.use(function(req, res, next) {
   if (req.isAuthenticated() && req.user.isAdmin) {
     User.findById(req.user._id, function(err, user) {
@@ -44,32 +82,39 @@ app.use(function(req, res, next) {
   }
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/alsDB", {useNewUrlParser: true});
+// mongoose.connect("mongodb://127.0.0.1:27017/alsDB", {useNewUrlParser: true});
+// async function main() {
 
-const usersSchema = new mongoose.Schema ({
-    name: String,
-    lastname: String,
-    email: String,
-    phone: Number,
-    username: String,
-    password: String,
-    isAdmin: { type: Boolean, default: false },
-    patients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patient', autopopulate: true }]
-});
+//   await mongoose.connect("mongodb+srv://admin-anastasis:Sf6DPj6QbS4BfK4@als.eudcbjz.mongodb.net/alsDB") 
+//   console.log("Connected");
+//   }
+
+
+    
+
+
+
+
 
 const patientsSchema = new mongoose.Schema({
   fname: String,
   lname: String,
   address: String,
   sex: String,
-  age: String,
+  age: Number,
   ssn: String,
-  phone: String,
-  fv: Date
+  phone: Number,
+  fv: Date,
+  allergies: String,
+  medications: String,
+  mutations: String,
+  famhistory: String,
+  medhistory: String,
+  onset: Date
 });
 
 const surveysSchema = new mongoose.Schema({
-  patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true},
+  patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: false},
   speech: {type: String, required: true},
   salivation: {type: String, required: true},
   swallowing: {type: String, required: true},
@@ -82,28 +127,16 @@ const surveysSchema = new mongoose.Schema({
   dyspnea: {type: String, required: true},
   orthopnea: {type: String, required: true},
   respiratoryInsufficiency: { type: String, required: true },
-  date: {type: Date, default: Date.now, get: (val) => moment(val).format("DD/MM/YYYY")}});
+  date: {type: Date, default: Date.now, get: (val) => moment(val).format("DD/MM/YYYY")},
+  totalScore: { type: Number, required: true },
+  percentage: { type: String, required: true }});
 
 
-usersSchema.plugin(passportLocalMongoose);
-usersSchema.plugin(findOrCreate);
 
-
-const User = mongoose.model("User", usersSchema);
 const Patient = mongoose.model("Patient", patientsSchema);
 const Survey = mongoose.model('Survey', surveysSchema);
 
-passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, done){
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done){
-  User.findById(id, function(err, user){
-    done(err, user);
-  });
-});
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -194,23 +227,65 @@ app.get("/logout", function(req, res, next){
       res.redirect('/');
     });
   });
-
-
-  app.post("/sign-up", function(req, res){
-    User.register({username: req.body.username, name: req.body.fname,
-       lastname: req.body.lname, email: req.body.email, phone: req.body.number, 
-       FV: req.body.firstv, Age: req.body.age, active: false}, 
-        req.body.password, function(err, user) {
+  app.post("/sign-up", function(req, res) {
+    // Check if an admin user already exists in the database
+    User.findOne({ isAdmin: true }, function(err, existingAdmin) {
       if (err) {
         console.log(err);
-        res.redirect("/sign-up")
-      } else {
-        passport.authenticate("local")(req, res, function(){
-          res.render("success");
-        });
+        res.redirect("/sign-up");
+        return;
       }
+  
+      if (existingAdmin) {
+        // An admin user already exists in the database, so set isAdmin to false for the new user
+        req.body.isAdmin = false;
+      } else {
+        // No admin user exists, so set isAdmin to true for the new user
+        req.body.isAdmin = true;
+      }
+  
+      // Use the isAdmin field in the User registration logic
+      User.register(
+        {
+          username: req.body.username,
+          name: req.body.fname,
+          lastname: req.body.lname,
+          email: req.body.email,
+          phone: req.body.number,
+          FV: req.body.firstv,
+          Age: req.body.age,
+          isAdmin: req.body.isAdmin,
+        },
+        req.body.password,
+        function(err, user) {
+          if (err) {
+            console.log(err);
+            res.redirect("/sign-up");
+          } else {
+            passport.authenticate("local")(req, res, function() {
+              res.render("success");
+            });
+          }
+        }
+      );
     });
   });
+
+  // app.post("/sign-up", function(req, res){
+  //   User.register({username: req.body.username, name: req.body.fname,
+  //      lastname: req.body.lname, email: req.body.email, phone: req.body.number, 
+  //      FV: req.body.firstv, Age: req.body.age, active: false}, 
+  //       req.body.password, function(err, user) {
+  //     if (err) {
+  //       console.log(err);
+  //       res.redirect("/sign-up")
+  //     } else {
+  //       passport.authenticate("local")(req, res, function(){
+  //         res.render("success");
+  //       });
+  //     }
+  //   });
+  // });
 
   
   app.post("/add-pat", function(req, res){
@@ -223,7 +298,13 @@ app.get("/logout", function(req, res, next){
        age: req.body.age,
        ssn: req.body.ssn,
        phone: req.body.phone,
-       fv:req.body.firstv
+       fv: req.body.firstv,
+       allergies: req.body.allergies,
+       medications: req.body.medications,
+       mutations: req.body.genmutations,
+       famhistory: req.body.familyhistory,
+       medhistory: req.body.medhistory,
+       onset: req.body.onset
     });
   
     patient.save(function(err, savedPatient){
@@ -247,22 +328,28 @@ app.get("/logout", function(req, res, next){
     });
   });
 
-app.post("/patients/:id", function(req, res) {
-  const patientId = req.params.id;
-  User.findOneAndUpdate(
-    { _id: req.user._id }, 
-    { $pull: { patients: patientId } },
-    { new: true }
-  )
-  .populate('patients') 
-  .exec(function(err, user) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect('/patients');
-    }
+  app.post("/patients/:id", function(req, res) {
+    const patientId = req.params.id;
+      User.findOneAndUpdate(
+      { _id: req.user._id },
+      { $pull: { patients: patientId } },
+      { new: true }
+    )
+      .populate('patients')
+      .exec(function(err, user) {
+        if (err) {
+          res.status(500).send("Error updating user");
+        } else {
+          Patient.findByIdAndRemove(patientId, function(err) {
+            if (err) {
+              res.status(500).send("Error deleting patient");
+            } else {
+              res.redirect('/patients');
+            }
+          });
+        }
+      });
   });
-});
 
 
 app.get("/history/:id", async function(req, res) {
@@ -290,30 +377,33 @@ const userId = req.user.id;
 
 
 app.post("/survey", function(req, res){
-    const survey = new Survey ({
-      patient: req.body.patientId,
-      speech: req.body["speech"],
-      salivation: req.body["salivation"],
-      swallowing: req.body["swallowing"],
-      handwriting: req.body["handwriting"],
-      cuttingFood: req.body["cutting-food"],
-      dressingHygiene: req.body["dressing-hygiene"],
-      turningInBed: req.body["turning-in-bed"],
-      walking: req.body["walking"],
-      climbingStairs: req.body["climbing-stairs"],
-      dyspnea: req.body["dyspnea"],
-      orthopnea: req.body["orthopnea"],
-      respiratoryInsufficiency: req.body["respiratory-insufficiency"],
-    });
+  const survey = new Survey({
+    patient: req.body.patientId,
+    speech: req.body["speech"],
+    salivation: req.body["salivation"],
+    swallowing: req.body["swallowing"],
+    handwriting: req.body["handwriting"],
+    cuttingFood: req.body["cutting-food"],
+    dressingHygiene: req.body["dressing-hygiene"],
+    turningInBed: req.body["turning-in-bed"],
+    walking: req.body["walking"],
+    climbingStairs: req.body["climbing-stairs"],
+    dyspnea: req.body["dyspnea"],
+    orthopnea: req.body["orthopnea"],
+    respiratoryInsufficiency: req.body["respiratory-insufficiency"],
+    totalScore: req.body["score"],
+    percentage: req.body["percentage"]
+  });
 
-    survey.save(function(err, savedSurvey){
-      if (err){
-          console.log(err);
-      } else {
-        res.redirect("/survey");
-      }
+  survey.save(function(err, savedSurvey){
+    if (err){
+      console.log(err);
+    } else {
+      res.redirect("/survey");
+    }
+  });
 });
-});
+
   app.post("/log-in", function(req, res, next) {
     passport.authenticate('local', function(err, user, info) {
       if (err) { return next(err); }
@@ -331,7 +421,13 @@ app.post("/survey", function(req, res){
       }
     });
   });
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
   });
-  
+} catch (error) {
+  console.error("Error connecting to MongoDB Atlas:", error);
+}
+}
+
+
+connectToDB();
